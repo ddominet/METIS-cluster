b41c2da50ecad65bf8babfd359c3243733b7610f
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2021-04-19 15:26:28 +0200

    Update nebula_install.sh

--- nebula_install.sh
+++ nebula_install.sh
@@ -50,20 +50,20 @@ fi
 if [ ! -d /etc/nebula ]; then
     sudo mkdir /etc/nebula
 fi
-if [ ! -d /home/cluster/metis_resources ]; then
-    mkdir /home/cluster/metis_resources
+if [ ! -d /tmp/metis_resources ]; then
+    mkdir /tmp/metis_resources
 fi
-if [ -d /home/cluster/metis_resources ]; then
-    git clone https://github.com/ddominet/METIS-cluster /home/cluster/metis_resources
+if [ -d /tmp/metis_resources ]; then
+    git clone https://github.com/ddominet/METIS-cluster /tmp/metis_resources
 fi
 if [ ! -f /etc/nebula/nebula ]; then
-    sudo cp /home/cluster/metis_resources/nebula /etc/nebula/
+    sudo cp /tmp/metis_resources/nebula /etc/nebula/
 fi
 if [ ! -f /etc/nebula/config.yaml ]; then
-    sudo cp /home/cluster/metis_resources/config.yaml /etc/nebula/
+    sudo cp /tmp/metis_resources/config.yaml /etc/nebula/
 fi
 if [ ! -f /etc/nebula/ca.crt ]; then
-    sudo cp /home/cluster/metis_resources/ca.crt /etc/nebula/
+    sudo cp /tmp/metis_resources/ca.crt /etc/nebula/
 fi
 if [ ! -f /etc/nebula/host.crt ]; then
     sudo mv $host_crt /etc/nebula/host.crt
@@ -75,11 +75,11 @@ fi
 # run as service
 
 if [ ! -f /etc/nebula/nebula-start.sh ]; then
-    sudo cp /home/cluster/metis_resources/nebula-start.sh /etc/nebula
+    sudo cp /tmp/metis_resources/nebula-start.sh /etc/nebula
     sudo chmod +x /etc/nebula/nebula-start.sh
 fi
 if [ ! -f /etc/systemd/system/nebula-start.service ]; then
-    sudo cp /home/cluster/metis_resources/nebula-start.service /etc/systemd/system/
+    sudo cp /tmp/metis_resources/nebula-start.service /etc/systemd/system/
     sudo chmod 644 /etc/systemd/system/nebula-start.service
     sudo systemctl daemon-reload
     sudo systemctl enable nebula-start.service
