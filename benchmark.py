f5c0855aff1ae4847613acf559978ba79c9039cd
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2021-05-02 16:35:03 +0200

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -9,7 +9,7 @@ from tqdm import tqdm, trange
 TEST_NUM_NODES = 22
 MAX_ACTORS_IN_CLUSTER = 22
 MAX_RUNNING_TASKS_IN_CLUSTER = 22
-MAX_PLACEMENT_GROUPS = 22
+MAX_PLACEMENT_GROUPS = 25
 MAX_NUM_NODES = 25
 
 
@@ -22,7 +22,7 @@ def num_alive_nodes():
 
 
 def scale_to(target):
-    while num_alive_nodes() != target:
+    while num_alive_nodes() >= target:
         ray.autoscaler.sdk.request_resources(bundles=[{"node": 1}] * target)
         print(f"Current # nodes: {num_alive_nodes()}, target: {target}")
         print("Waiting ...")
